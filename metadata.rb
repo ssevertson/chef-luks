3016a8869d3b24e7de81fff43ba02d4b6d8f52a3
Scott Severtson <scott@severtson.us>
2014-04-30 14:53:35 -0400

    Bump version number, as exists/open support changes behavior

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@intoxitrack.net'
 license          'Apache 2.0'
 description      'Installs/Configures LUKS block encryption'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 %w{redhat fedora debian}.each do |os|
   supports os
